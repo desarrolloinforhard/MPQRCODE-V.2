e62d85bf82425f0a3bcff8d5b8649b55f0d912bb
M1S43L10 <misaelr893@gmail.com>
2024-04-15 09:00:45 -0300

    V2.5.5

--- image_path.py
+++ image_path.py
@@ -1,24 +1,24 @@
 import os
 path_script = os.path.dirname(os.path.abspath(__file__))
 def Icono_MercadoPago_Blue():
-    return os.path.join(path_script, 'Icono-MercadoPago-Blue.ico')
+    return os.path.join(path_script, 'assets', 'Icono-MercadoPago-Blue.ico')
 
 def LOGO_INFORHARD_horizontal():
-    return os.path.join(path_script, 'LOGO_INFORHARD-horizontal.png')
+    return os.path.join(path_script, 'assets', 'LOGO_INFORHARD-horizontal.png')
 
 def LOGO_INFORHARD():
-    return os.path.join(path_script, 'LOGO_INFORHARD.png')
+    return os.path.join(path_script, 'assets', 'LOGO_INFORHARD.png')
 
 def LOGO_MP():
-    logo = os.path.join(path_script, 'LOGO_MP.png')
+    logo = os.path.join(path_script, 'assets', 'LOGO_MP.png')
     return logo
 
 def TACHO_BASURA():
-    return os.path.join(path_script, 'tacho_basura.png')
+    return os.path.join(path_script, 'assets', 'tacho_basura.png')
 
 
 def REEMPLAZAR():
-    return os.path.join(path_script, 'reemplazar.png')
+    return os.path.join(path_script, 'assets', 'reemplazar.png')
 
 def AGREGAR():
-    return os.path.join(path_script, 'agregar.png')
+    return os.path.join(path_script, 'assets', 'agregar.png')
